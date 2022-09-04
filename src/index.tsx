7b5d5da1974c576446b394dcb2857688033a21bf
palenquiana <ludmilapalenque@hotmail.com>
2022-09-04 13:11:54 -0300

    feat: outlet for app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
-    <Route>
-      <App />
-    </Route>
+    <Routes>
+      <Route path="/" element={<Outlet />}></Route>
+    </Routes>
   </BrowserRouter>
 );
