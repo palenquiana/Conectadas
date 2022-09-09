045353a4109cfd53fbe73bb60b60fa66646ad851
palenquiana <ludmilapalenque@hotmail.com>
2022-09-09 16:18:28 -0300

    create login page

---
+++ src/components/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../common";
+
+const LoginPage = () => {
+  return (
+    <Layout hideNav page="login">
+      <> Login form</>
+    </Layout>
+  );
+};
+export { LoginPage };
