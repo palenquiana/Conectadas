dbae4846703cecfb8f2c9b023c27770a7cb5d9cc
palenquiana <ludmilapalenque@hotmail.com>
2022-10-01 10:46:04 -0300

    feat: axios

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,71 +1,31 @@
 import { User, UserPayload } from "../../types";
 import { mapToArray } from "../helpers";
+import { apiDB } from "../../utils";
 
 const add = async (user: UserPayload) => {
-  const options = {
-    method: "POST",
-    body: JSON.stringify(user),
-    Headers: {
-      contentType: "application/json",
-    },
-  };
-
-  const response = await fetch(
-    "https://conectadas-5487f-default-rtdb.firebaseio.com/users.json",
-    options
-  );
-  const data = await response.json();
-
-  return mapToArray(data);
+  apiDB.post("/users.json", JSON.stringify(user));
 };
 
 const getAll = async (): Promise<User[]> => {
-  const response = await fetch(
-    "https://conectadas-5487f-default-rtdb.firebaseio.com/users.json"
-  );
-  const data = await response.json();
-
-  return mapToArray(data);
+  const response = await apiDB.get("/users.json");
+  return mapToArray(response.data);
 };
 
 const get = async (id: string) => {
-  const response = await fetch(
-    `https://conectadas-5487f-default-rtdb.firebaseio.com/users/${id}.json`
-  );
-  const data = await response.json();
+  const response = await apiDB.get(`users/${id}.json`);
 
-  return data;
+  return mapToArray(response.data);
 };
 
 const patch = async (id: string, payload: Partial<User>) => {
-  const options = {
-    method: "PATCH",
-    body: JSON.stringify(payload),
-    Headers: {
-      contentType: "application/json",
-    },
-  };
+  const response = await apiDB.patch(`/users/${id}.json`);
 
-  const response = await fetch(
-    `https://conectadas-5487f-default-rtdb.firebaseio.com/users/${id}.json`,
-    options
-  );
-  const data = await response.json();
-
-  return mapToArray(data);
+  return mapToArray(response.data);
 };
 
 const remove = async (id: string) => {
-  const options = {
-    method: "DELETE",
-  };
+  const response = await apiDB.delete(`/users/${id}.json`);
 
-  const response = await fetch(
-    `https://conectadas-5487f-default-rtdb.firebaseio.com/users/${id}.json`,
-    options
-  );
-  const data = await response.json();
-
-  return data;
+  return response.data;
 };
 export const usersApi = { add, getAll, get, remove, patch };
