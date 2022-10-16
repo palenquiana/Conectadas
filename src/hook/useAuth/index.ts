a8f2547a874d774b71ab242d536d0ed192571ad6
palenquiana <ludmilapalenque@hotmail.com>
2022-10-16 11:52:44 -0300

    test send data

--- src/hook/useAuth/index.ts
+++ src/hook/useAuth/index.ts
@@ -5,16 +5,15 @@ import { AuthContext } from "../../context";
 
 const useAuth = () => {
   const { me, setCurrentUser } = useContext(AuthContext);
-
+  useEffect(() => {
+    loginWithToken();
+  }, []);
   const setUserToken = async (id: string) => {
     const newToken = Math.random().toString(36).substring(2);
     const resp = await usersApi.patch(id, { sessionToken: newToken });
     return resp ? newToken : null;
   };
   const login = async ({ email, pass }: LoginFormType) => {
-    useEffect(() => {
-      loginWithToken();
-    }, []);
     const users = await usersApi.getAll();
     if (users) {
       const logged = users.find(
@@ -28,34 +27,24 @@ const useAuth = () => {
           setCurrentUser(logged);
         }
       }
-
-      const loginWithToken = async () => {
-        const users = await usersApi.getAll();
-        const storedToken = localStorage.getItem("user-token");
-        const logged = users.find((user) => user.sessionToken === storedToken);
-        if (!me && logged) {
-          setCurrentUser(logged);
-        }
-      };
-      useEffect(() => {
-        loginWithToken();
-      }, []);
     }
-
-    const loginWithToken = async () => {
-      const users = await usersApi.getAll();
-      const storedToken = localStorage.getItem("user-token");
-      const logged = users.find((user) => user.sessionToken === storedToken);
-      if (!me && logged) {
-        setCurrentUser(logged);
-      }
-    };
-    const logout = (id: string) => {
-      usersApi.patch(id, { sessionToken: null });
-      setCurrentUser(undefined);
-    };
-
-    return { login, logout };
   };
+  const loginWithToken = async () => {
+    const users = await usersApi.getAll();
+    const storedToken = localStorage.getItem("user-token");
+    const logged = users.find((user) => user.sessionToken === storedToken);
+    if (!me && logged) {
+      setCurrentUser(logged);
+    }
+  };
+  const logout = (id: string) => {
+    usersApi.patch(id, { sessionToken: null });
+    setCurrentUser(undefined);
+  };
+  const sigup = async (user: SignUpPayload) => {
+    const addUser = await usersApi.add(user);
+    return addUser;
+  };
+  return { login, logout, sigup };
 };
 export { useAuth };
